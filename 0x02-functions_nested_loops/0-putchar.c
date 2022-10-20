0621a60fd801655128d29fd87561312ef32cdbb9
Royal07T <timmyroyalty@gmail.com>
2022-10-19 18:50:52 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - function to print
- * Return: 0
+ * main - prints _putchar and a new line
+ * Return: Always 0
  */
 int main(void)
 {
-	char nested[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	unsigned int c;
-
-	for (c = 0; c < sizeof(nested); c++)
-
-	{
-		_putchar(nested[c]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }

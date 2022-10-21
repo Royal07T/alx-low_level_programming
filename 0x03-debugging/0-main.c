dacf6c99f6813e284aa713c7cdd1d79b7a6396c4
Royal07T <timmyroyalty@gmail.com>
2022-10-20 17:54:28 -0700

    0 TasK

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int i = 0;
+	postitive_or_negative(i);
+
+	return 0;
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * File- main.h
+ * @i: integer parameter
+ * Description:  Header file declaring functions in 0x03-debugging directory
+ */
+
+void positive_or_negative(int i)
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif

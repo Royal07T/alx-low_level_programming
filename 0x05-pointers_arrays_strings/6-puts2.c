eb1b4d3c7b1b0fed8fad4ee4182d771b3e26339a
Royal07T <timmyroyalty@gmail.com>
2022-10-24 18:14:32 -0700

    Tsk 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - function
+ * @str: parameter
+ * Return: Value
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+	int y = 0;
+
+	while (str[x++])
+	{
+		y++;
+	}
+
+	for (x = 0; x < y; x += 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}

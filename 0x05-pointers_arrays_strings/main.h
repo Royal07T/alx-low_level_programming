8024993664054d09457b13f0e94a98fb4df80625
Royal07T <timmyroyalty@gmail.com>
2022-10-24 17:35:24 -0700

    Task 1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,16 @@
 
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef  MAIN_H
+#define  MAIN_H
+
+/**
+ * File - main.h
+ * description: Header file
+ * _putchar - function
+ * @c: parameter
+ * Return: value
+ */
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif

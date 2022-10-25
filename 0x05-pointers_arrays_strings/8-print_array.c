01795f32b0ce9e77d16f9f66a2326116cc8b04fd
Royal07T <timmyroyalty@gmail.com>
2022-10-25 06:42:17 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - function
+ * @a: parameter
+ * @n: parameter
+ * Return: Value
+ */
+
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	for (; x < n; x++)
+	{
+		printf("%d", *(a + x));
+
+		if (x != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+

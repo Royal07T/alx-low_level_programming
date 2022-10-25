0fc229806111d1b69aa76f1fc9126478e27c2035
Royal07T <timmyroyalty@gmail.com>
2022-10-24 18:04:08 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function
+ * @s: parameter
+ * Return: Value
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

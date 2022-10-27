bf3299c1c17180394103939ffc63c3305e6479da
Royal07T <timmyroyalty@gmail.com>
2022-10-27 01:55:28 -0700

    Update fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - main
+ * Return: value
+ */
+
+void times_table(void)
+{
+	int num, multiply, value;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (multiply = 1; multiply <= 9; multiply++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			value = num * multiply;
+
+			if (value <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((value / 10) + '0');
+			}
+			_putchar((value % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
